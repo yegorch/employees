9bb3173036fe408f81cb2e72e2c4375cc16508fd
yegorch <yegor.rb@gmail.com>
2021-10-26 20:11:10 +0400

    ---

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -3,6 +3,7 @@ import EmployeesListItem from "../employees-list-item/employees-list-item";
 import './employees-list.css';
 
 const EmployeesList = ({data}) => {
+    
     const elements = data.map(item => {
         return (
             <EmployeesListItem name={item.name} salary={item.salary}/>
